b6001a9b1adad63cf18a3c13e4744b283bfd86ae
BwizLabs <brandon.r.russell.1@gmail.com>
2019-05-25 15:18:36 -0400

    Added cross_origin

--- src/main/kotlin/app/bwizlabs/dayshout/controller/ShoutController.kt
+++ src/main/kotlin/app/bwizlabs/dayshout/controller/ShoutController.kt
@@ -1,7 +1,5 @@
 package app.bwizlabs.dayshout.controller
-
 import org.springframework.beans.factory.annotation.Autowired
-
 import app.bwizlabs.dayshout.repo.ShoutRepository
 import app.bwizlabs.dayshout.model.Shout
 import org.springframework.web.bind.annotation.*
@@ -14,6 +12,7 @@ class ShoutController {
     @Autowired
     lateinit var repository: ShoutRepository
 
+    @CrossOrigin
     @RequestMapping("/save")
     fun save(): String {
         repository.save(Shout( shoutIp = "test", shoutEntry = "test", shoutLat = 0.0, shoutLong = 0.0))
@@ -21,26 +20,19 @@ class ShoutController {
         return "Done"
     }
 
+    @CrossOrigin
     @RequestMapping("/findall")
     fun findAll() = repository.findAll()
 
+    @CrossOrigin
     @RequestMapping("/findbyid/{id}")
     fun findById(@PathVariable id: Long)
             = repository.findById(id)
 
+    @CrossOrigin
     @RequestMapping("findByShout/{shoutEntry}")
     fun findByShout(@PathVariable shoutEntry: String)
             = repository.findByShoutEntry(shoutEntry)
 
- /*   @PostMapping
-    @RequestMapping(value = "/newShout")
-    fun createUser(@RequestBody newShout: Shout): Shout {
-        val shout = Shout(
-                shoutDate = LocalDate.now(),
-                shoutEntry = newShout.shoutEntry,
-                shoutLat = newShout.shoutLat,
-                shoutLong = newShout.shoutLong
-        )
-        return shout
-    }*/
+
 }
