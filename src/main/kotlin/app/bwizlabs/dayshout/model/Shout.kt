0300eabc265f856e1c9df991d372ce8ca4b65e18
BwizLabs <brandon.r.russell.1@gmail.com>
2019-06-08 11:29:53 -0400

    Image upload experiment

--- src/main/kotlin/app/bwizlabs/dayshout/model/Shout.kt
+++ src/main/kotlin/app/bwizlabs/dayshout/model/Shout.kt
@@ -1,14 +1,16 @@
 package app.bwizlabs.dayshout.model
 
 
+
 import java.time.LocalDate
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
+import javax.persistence.*
+import javax.xml.bind.DatatypeConverter
 
 @Entity
 data class Shout(
+
+        @Lob
+        val shoutImage: ByteArray? = null,
         val shoutIp: String,
         val shoutDate: LocalDate = LocalDate.now(),
         val shoutEntry: String,
@@ -17,5 +19,14 @@ data class Shout(
         @Id @GeneratedValue(strategy = GenerationType.AUTO)
         val id: Long = -1) {
         private constructor() : this( shoutIp = "", shoutEntry = "", shoutLat = 0.0, shoutLong = 0.0)
+
+
+/*        fun toStreamingURI() : String {
+                //We need to encode the byte array into a base64 String for the browser
+                val base64 = DatatypeConverter.printBase64Binary(shoutImage)
+
+                //Now just return a data string. The Browser will know what to do with it
+                return "data:$mime;base64,$base64"
+        }*/
 }
 
